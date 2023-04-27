476062a6483548efd60c30917f2df9a49db10357
July Tao <tjjsilwolf@gmail>
2023-04-27 20:10:08 +0800

    Initial Commit

---
+++ NetworkAndURLSession/NetworkAndURLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkAndURLSessionApp.swift
+//  NetworkAndURLSession
+//
+//  Created by july on 2023/4/27.
+//
+
+import SwiftUI
+
+@main
+struct NetworkAndURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
